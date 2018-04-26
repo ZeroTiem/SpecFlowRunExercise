cd6da4dc72603018ff354450ab8a10c347a9a496
VS_Online <maxhank22@gmail.com>
2018-04-27 03:30:43 +0800

    加入專案檔案。

---
+++ SpecFlowRunExerciseTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecFlowRunExercise.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowRunExercise.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
